5fdd67d96efde29fe4c1315b4ae70329f1ae87d5
Sudhir Sahoo <SudhirSahoo@sudhircentos.attlocal.net>
2020-04-09 22:33:35 -0500

    changed Service

--- src/main/java/com/sk/kms/service/AssociateService.java
+++ src/main/java/com/sk/kms/service/AssociateService.java
@@ -1,6 +1,7 @@
 package com.sk.kms.service;
 
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,10 +24,21 @@ public class AssociateService  {
 	 
 	public Associate create(Associate associate) {
 		if(associate == null || associate.getId() == 0) {
-			associate = new Associate(1001, "Brain", "Turner");
+			int i = getRandomNumberInRange(1000, 9999);
+			associate = new Associate(i, associate.getFirstName(), associate.getLastName());
 		}
 		associate = associateJpaRepository.save(associate);
 		return associate; 
+	}
+	
+	private static int getRandomNumberInRange(int min, int max) {
+
+		if (min >= max) {
+			throw new IllegalArgumentException("max must be greater than min");
+		}
+
+		Random r = new Random();
+		return r.nextInt((max - min) + 1) + min;
 	}
 
 }
